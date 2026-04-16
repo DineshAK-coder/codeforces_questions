dbee8fcc91c316828fccb9d2297797012ebd9cf6
DineshAK-coder <dineshkuttan78@gmail.com>
2026-04-16 15:00:07 +0530

    231A.Team problem. README.md score updated to 3 on the 800-1000 section

---
+++ 800/231A.Team.cpp
@@ -0,0 +1,24 @@
+/*One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
+
+This contest offers n problems to the participants. For each problem we know, which friend is sure about the solution. Help the friends find the number of problems for which they will write a solution.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n,count;
+  count = 0;
+  cin>>n;
+  while(n--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b+c >= 2){
+      count ++;
+    }
+  }
+  cout<<count<<'\n';
+  return 0;
+}
