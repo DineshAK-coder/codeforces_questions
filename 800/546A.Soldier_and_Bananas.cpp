0b8e06fdba1c4eb4b553d0ea39e2c6de1bce8bf3
DineshAK-coder <dineshkuttan78@gmail.com>
2026-04-26 11:20:24 +0530

    Solved 546A Soldier and Bananas problem

---
+++ 800/546A.Soldier_and_Bananas.cpp
@@ -0,0 +1,15 @@
+/* proble link : https://codeforces.com/problemset/problem/546/A */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int k,n,w,amount = 0;
+  cin>>k>>n>>w;
+  amount = ((w*(w+1))/2) * k;
+  cout<<amount - n<<endl;
+  return 0;
+}
