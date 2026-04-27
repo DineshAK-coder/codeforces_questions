f9a4d41836a877be877a038966cd4f36fcf5b7e5
DineshAK-coder <dineshkuttan78@gmail.com>
2026-04-27 23:27:45 +0530

    Getting wrong answer for 110A. Will try again tomorrow

---
+++ 800/110A.Nearly_lucky_number.cpp
@@ -0,0 +1,24 @@
+/* problem link : https://codeforces.com/problemset/problem/110/A */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int num,count = 0;
+  bool status = false;
+  cin>>num;
+  while(num != 0){
+    int x = num % 10;
+    num /= 10;
+    if(x == 7 | x == 4){
+      status = true;
+      count++;
+    }
+  }
+  string result = (status & (count  == 4 | count  == 7 )) ? "YES" : "NO";
+  cout<<result<<endl;
+  return 0;
+}
